49f98a55f321fc1fa6e4778dbfa7e39f81ea42c6
anisbaharin <2022901147@student.uitm.edu.my>
2023-12-09 07:13:18 +0800

    editProfile

--- app/src/main/java/com/example/signuploginfirebase/EditProfileActivity.java
+++ app/src/main/java/com/example/signuploginfirebase/EditProfileActivity.java
@@ -13,9 +13,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class EditProfileActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -24,16 +29,22 @@ public class EditProfileActivity extends AppCompatActivity implements Navigation
     DrawerLayout drawerLayout;
 
     private FirebaseAuth mAuth;
+    private EditText editUsername, editPhone, editEmail;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_profile_activity);
 
+        db = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar);
+        editUsername = findViewById(R.id.textViewUsername);
+        editPhone = findViewById(R.id.textViewPhone);
+        editEmail = findViewById(R.id.textViewEmail);
 
         setSupportActionBar(toolbar);
 
@@ -44,6 +55,53 @@ public class EditProfileActivity extends AppCompatActivity implements Navigation
         navigationView.setNavigationItemSelectedListener(this);
 
         navigationView.setCheckedItem(R.id.nav_home);
+        FirebaseUser currentUser=mAuth.getCurrentUser();
+
+        if (currentUser != null) {
+            String userID = currentUser.getUid();
+
+            db.collection("user")
+                    .document(userID)
+                    .get()
+                    .addOnSuccessListener(documentSnapshot -> {
+                        if (documentSnapshot.exists()) {
+                            String username = documentSnapshot.getString("username");
+                            String email = documentSnapshot.getString("email");
+                            String phone = documentSnapshot.getString("phone");
+
+                            editUsername.setText(username);
+                            editEmail.setText(email);
+                            editPhone.setText(phone);
+                        }
+                    })
+                    .addOnFailureListener(e -> {
+                        Toast.makeText(getApplicationContext(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    });
+        }
+
+        Button saveButton = findViewById(R.id.retrievebtn);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newUsername = editUsername.getText().toString();
+                String newEmail = editEmail.getText().toString();
+                String newPhone = editPhone.getText().toString();
+
+                updateProfile(currentUser.getUid(),newUsername,newEmail,newPhone);
+            }
+        });
+    }
+
+    private void updateProfile(String userID, String newUsername, String newEmail, String newPhone){
+        db.collection("user")
+                .document(userID)
+                .update("username",newUsername,"email", newEmail, "phone", newPhone)
+                .addOnSuccessListener(aVoid -> {
+                    finish();
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(getApplicationContext(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
     }
 
     @Override
